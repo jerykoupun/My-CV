754c6e9bc8b925aa02ba6eb5dc7bae2c9bca8088
jery koupun <s22129077@student.unklab.ac.id>
2024-11-17 02:27:32 +0800

    Adding firebase in Intro

--- src/components/Main/Intro/index.js
+++ src/components/Main/Intro/index.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 import Image1 from "../../../assets/img/avatar.jpg";
 
 const Intro = () => {
+  const [intro, setIntro] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const introRef = ref(db, "intro");
+    onValue(introRef, (snapshot) => {
+      const data = snapshot.val();
+      setIntro(data);
+    });
+  }, []);
   return (
     <section className="cv-block block-intro border-bottom">
       <div className="container">
         <div className="avatar">
-          <img className="img-fluid rounded-circle" src={Image1} />
+          <img
+            className="img-fluid rounded-circle"
+            src={`data:image/jpg;base64,${intro.image}`}
+          />
         </div>
-        <p>
-          Hello! I am <strong>Jery Koupun</strong>.I am a student at the Faculty
-          of Computer Science at Universitas Klabat, majoring in Information
-          Systems. I have a strong interest in design and analysis.
-        </p>
+        <p>{intro.aboutme}</p>
         <a href="#" className="btn btn-outline-success">
-          Hire Me
+          {intro.button}
         </a>
       </div>
     </section>
