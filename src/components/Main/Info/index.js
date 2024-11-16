4a907731a688dd04cd8b60f1e6269b76daa9c19a
jery koupun <s22129077@student.unklab.ac.id>
2024-11-17 03:14:04 +0800

    Adding firebase in info

--- src/components/Main/Info/index.js
+++ src/components/Main/Info/index.js
@@ -1,96 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 const Info = () => {
+  const [info, setInfo] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const infoRef = ref(db, "info");
+    onValue(infoRef, (snapshot) => {
+      const data = snapshot.val();
+      setInfo(data);
+    });
+  }, []);
   return (
     <section className="cv-block info">
       <div className="container">
         <div className="work-experience group" id="portofolio">
-          <h2 className="text-center">Portofolio</h2>
+          <h2 className="text-center">{info.portofolio}</h2>
           <div className="item">
             <div className="row">
               <div className="col-md-6">
-                <h3>Web Klabat Set Up</h3>
-                <h4 className="organization">Klabat Set Up</h4>
+                <h3>{info.web1}</h3>
+                <h4 className="organization">{info.web1jdl}</h4>
               </div>
               <div className="col-md-6">
-                <time className="period">2024</time>
+                <time className="period">{info.web1jdl}</time>
               </div>
             </div>
-            <p className="text-muted">
-              I created the Klabat Set Up website in 2024 to provide outdoor
-              enthusiasts with high-quality mountain hiking equipment and
-              apparel. The platform is designed to cater to adventurers by
-              offering a seamless shopping experience, detailed product
-              information, and a secure blockchain-based transaction system.
-            </p>
+            <p className="text-muted">{info.web1deskripsi}</p>
           </div>
           <div className="item">
             <div className="row">
               <div className="col-md-6">
-                <h3>Web Filkom Day</h3>
-                <h4 className="organization">Filkom Day.</h4>
+                <h3>{info.web2}</h3>
+                <h4 className="organization">{info.web2jdl}</h4>
               </div>
               <div className="col-md-6">
-                <time className="period">2023</time>
+                <time className="period">{info.web2thn}</time>
               </div>
             </div>
-            <p className="text-muted">
-              I created the Filkom Day website in 2023 to simplify the
-              registration process for Filkom Day and serve as a place where
-              Filkom students can view galleries and photos from the event. The
-              website is designed to allow students to easily access event
-              information, register online, and enjoy visual documentation of
-              the exciting moments from the event.
-            </p>
+            <p className="text-muted">{info.web2deskripsi}</p>
           </div>
         </div>
         <div className="education group" id="education">
-          <h2 className="text-center">Education</h2>
+          <h2 className="text-center">{info.education}</h2>
           <div className="item">
             <div className="row">
               <div className="col-md-6">
-                <h3>Vocational High School Graduate</h3>
-                <h4 className="organization">SMK Negeri 1 Wori</h4>
+                <h3>{info.edu1}</h3>
+                <h4 className="organization">{info.edu1jdl}</h4>
               </div>
               <div className="col-md-6">
-                <time className="period">2019 - 2022</time>
+                <time className="period">{info.edu1thn}</time>
               </div>
             </div>
-            <p className="text-muted">
-              I completed my education at SMK Negeri 1 Wori, majoring in
-              Computer Engineering and Networking. During my studies, I gained
-              practical skills and in-depth knowledge in computer systems,
-              network installation, as well as hardware and software
-              maintenance. This education provided me with a solid foundation to
-              tackle challenges in the field of information technology.
-            </p>
+            <p className="text-muted">{info.edu1deskripsi}</p>
           </div>
           <div className="item">
             <div className="row">
               <div className="col-md-6">
-                <h3>Student of Information Systems</h3>
-                <h4 className="organization">Universitas Klabat</h4>
+                <h3>{info.edu2}</h3>
+                <h4 className="organization">{info.edu2jdl}</h4>
               </div>
               <div className="col-md-6">
-                <time className="period">2022-2026</time>
+                <time className="period">{info.edu2thn}</time>
               </div>
             </div>
-            <p className="text-muted">
-              I'm currently pursuing my studies in Information Systems at
-              Universitas Klabat. This program allows me to develop a deep
-              understanding of system analysis, business process modeling, and
-              IT solutions. Throughout my education, I am gaining valuable
-              skills in both technical and analytical aspects of information
-              technology, preparing me for a career in this dynamic field.
-            </p>
+            <p className="text-muted">{info.edu2deskripsi}</p>
           </div>
         </div>
         <div className="group" id="skills">
           <div className="row">
             <div className="col-md-6">
               <div className="skills info-card">
-                <h2>Skills</h2>
-                <h3>HTML</h3>
+                <h2>{info.skil}</h2>
+                <h3>{info.skil1}</h3>
                 <div className="progress">
                   <div
                     className="progress-bar"
@@ -101,7 +84,7 @@ const Info = () => {
                     style={{ width: "90%" }}
                   ></div>
                 </div>
-                <h3>WORDPRES</h3>
+                <h3>{info.skil2}</h3>
                 <div className="progress">
                   <div
                     className="progress-bar"
@@ -112,7 +95,7 @@ const Info = () => {
                     style={{ width: "90%" }}
                   ></div>
                 </div>
-                <h3>JavaScript</h3>
+                <h3>{info.skil3}</h3>
                 <div className="progress">
                   <div
                     className="progress-bar"
@@ -127,13 +110,13 @@ const Info = () => {
             </div>
             <div className="col-md-6">
               <div className="contact-info info-card">
-                <h2>Contact Info</h2>
+                <h2>{info.contact}</h2>
                 <div className="row">
                   <div className="col-1">
                     <i className="ion-android-calendar icon" />
                   </div>
                   <div className="col-9">
-                    <span>9 Agustus 2004</span>
+                    <span>{info.contactthn}</span>
                   </div>
                 </div>
                 <div className="row">
@@ -141,7 +124,7 @@ const Info = () => {
                     <i className="ion-person icon" />
                   </div>
                   <div className="col-9">
-                    <span>Jery Koupun</span>
+                    <span>{info.contactname}</span>
                   </div>
                 </div>
                 <div className="row">
@@ -149,7 +132,7 @@ const Info = () => {
                     <i className="ion-ios-telephone icon" />
                   </div>
                   <div className="col-9">
-                    <span>08884620592</span>
+                    <span>{info.contactnum}</span>
                   </div>
                 </div>
                 <div className="row">
@@ -157,7 +140,7 @@ const Info = () => {
                     <i className="ion-at icon" />
                   </div>
                   <div className="col-9">
-                    <span>Jerykoupun@gmail.com</span>
+                    <span>{info.contactemail}</span>
                   </div>
                 </div>
               </div>
@@ -165,14 +148,8 @@ const Info = () => {
           </div>
         </div>
         <div className="hobbies group" id="hobbies">
-          <h2 className="text-center">Hobbies</h2>
-          <p className="text-center text-muted">
-            My main hobby is playing football. I love the energy, teamwork, and
-            strategy involved in the game. Its not only a fun way to stay active
-            and healthy but also an excellent way to develop discipline and
-            build camaraderie with others. Playing football brings excitement
-            and joy to my life.
-          </p>
+          <h2 className="text-center">{info.hoby}</h2>
+          <p className="text-center text-muted">{info.hobydeskripsi}</p>
         </div>
       </div>
     </section>
